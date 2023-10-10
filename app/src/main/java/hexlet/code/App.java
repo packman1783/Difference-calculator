5709b03fb1253f5885d6185e95808b43311be7e0
Saplitsyn Denis <packman17@mail.ru>
2023-10-10 20:45:09 +0300

    change App build.gradle (add picocli)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable {
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-v", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
