a68856a367465b420036c1fb6c1b5fb266652f11
Saplitsyn Denis <packman17@mail.ru>
2023-10-19 20:55:53 +0300

    change DifferTest

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -24,24 +24,12 @@ public class DifferTest {
 
     public static String getContent(String pathString) throws Exception {
         Path path = Paths.get(pathString);
-// newline problem for Unix \n and Windows \r\n
-        List<String> lines = Files.readAllLines(path);
-        return String.join("\n", lines);
+        return Files.readString(path);
     }
 
     @Test
     public void testJson() throws Exception {
         String actual = Differ.generate(jsonPathFile1, jsonPathFile2);
-        System.out.println(expectedJson);
-//        String expected = "{\n" +
-//                "  - follow: false\n" +
-//                "    host: hexlet.io\n" +
-//                "  - proxy: 123.234.53.22\n" +
-//                "  - timeout: 50\n" +
-//                "  + timeout: 20\n" +
-//                "  + verbose: true\n" +
-//                "}";
         assertThat(actual).isEqualTo(expectedJson);
     }
-
 }
