116c9bc65bfeb7bbcb89de4021b290220eefc56b
Saplitsyn Denis <packman17@mail.ru>
2023-10-19 20:56:56 +0300

    change DifferTest

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class DifferTest {
     static String expectedJson;
