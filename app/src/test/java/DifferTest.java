cfe5a0e76c09755feb48e40d63d329c7fece7d43
Saplitsyn Denis <packman17@mail.ru>
2023-10-23 21:15:21 +0300

    add .yml files test

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -11,14 +11,20 @@ import java.nio.file.Paths;
 
 public class DifferTest {
     static String expectedJson;
+    static String expectedYml;
     static String jsonPathFile1;
     static String jsonPathFile2;
+    static String ymlPathFile1;
+    static String ymlPathFile2;
 
     @BeforeAll
     public static void beforeAll() throws Exception {
         jsonPathFile1 = "src/test/resources/file1.json";
         jsonPathFile2 = "src/test/resources/file2.json";
         expectedJson = getContent("src/test/resources/expectedJson.txt");
+        ymlPathFile1 = "src/test/resources/file1.yml";
+        ymlPathFile2 = "src/test/resources/file2.yml";
+        expectedYml = getContent("src/test/resources/expectedYml.txt");
     }
 
     public static String getContent(String pathString) throws Exception {
@@ -30,5 +36,11 @@ public class DifferTest {
     public void testJson() throws Exception {
         String actual = Differ.generate(jsonPathFile1, jsonPathFile2);
         assertThat(actual).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void testYml() throws Exception {
+        String actual = Differ.generate(ymlPathFile1, ymlPathFile2);
+        assertThat(actual).isEqualTo(expectedYml);
     }
 }
