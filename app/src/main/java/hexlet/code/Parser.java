6c693daf3401e5d6f0e16b08ad124d9e55e1ab3f
Saplitsyn Denis <packman17@mail.ru>
2023-10-23 21:16:44 +0300

    add parse for .yml files

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,12 +2,24 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parse(String text) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(text, new TypeReference<>() { });
+    public static Map<String, Object> parseToMap(String contentFile, String fileFormat) throws Exception {
+
+        switch (fileFormat) {
+            case "json" -> {
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.readValue(contentFile, new TypeReference<Map<String, Object>>() { });
+            }
+
+            case "yml" -> {
+                ObjectMapper mapper = new YAMLMapper();
+                return mapper.readValue(contentFile, new TypeReference<Map<String, Object>>() { });
+            }
+            default -> throw new Exception("Unexpected format: " + fileFormat);
+        }
     }
 }
