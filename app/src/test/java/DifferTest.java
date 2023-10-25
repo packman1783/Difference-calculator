cd7066ce583f2e9e3c04060bbed8ca226404b66d
Saplitsyn Denis <packman17@mail.ru>
2023-10-25 20:15:44 +0300

    add test for "plain" style

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -10,10 +10,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DifferTest {
-    static String expectedJson;
-    static String expectedYml;
-    static String expectedTreeJson;
-    static String expectedTreeYml;
+    static String expectedSimpleComparing;
+    static String expectedStylish;
+    static String expectedPlain;
     static String jsonPathFile1;
     static String jsonPathFile2;
     static String ymlPathFile1;
@@ -27,17 +26,20 @@ public class DifferTest {
     public static void beforeAll() throws Exception {
         jsonPathFile1 = "src/test/resources/file1.json";
         jsonPathFile2 = "src/test/resources/file2.json";
-        expectedJson = getContent("src/test/resources/expectedJson.txt");
         ymlPathFile1 = "src/test/resources/file1.yml";
         ymlPathFile2 = "src/test/resources/file2.yml";
-        expectedYml = getContent("src/test/resources/expectedYml.txt");
+        expectedSimpleComparing = getContent("src/test/resources/expectedSimpleComparing.txt");
+
         jsonTreePathFile1 = "src/test/resources/fileTree1.json";
         jsonTreePathFile2 = "src/test/resources/fileTree2.json";
-        expectedTreeJson = getContent("src/test/resources/expectedTreeJson.txt");
         ymlTreePathFile1 = "src/test/resources/fileTree1.yml";
         ymlTreePathFile2 = "src/test/resources/fileTree2.yml";
-        expectedTreeYml = getContent("src/test/resources/expectedTreeYml.txt");
+        expectedStylish = getContent("src/test/resources/expectedStylish.txt");
+
+        expectedPlain = getContent("src/test/resources/expectedPlain.txt");
+
     }
+
 
     public static String getContent(String pathString) throws Exception {
         Path path = Paths.get(pathString);
@@ -47,24 +49,36 @@ public class DifferTest {
     @Test
     public void testJson() throws Exception {
         String actual = Differ.generate(jsonPathFile1, jsonPathFile2);
-        assertThat(actual).isEqualTo(expectedJson);
+        assertThat(actual).isEqualTo(expectedSimpleComparing);
     }
 
     @Test
     public void testYml() throws Exception {
         String actual = Differ.generate(ymlPathFile1, ymlPathFile2);
-        assertThat(actual).isEqualTo(expectedYml);
+        assertThat(actual).isEqualTo(expectedSimpleComparing);
     }
 
     @Test
-    public void testTreeJson() throws Exception {
-        String actual = Differ.generate(jsonTreePathFile1, jsonTreePathFile2);
-        assertThat(actual).isEqualTo(expectedTreeJson);
+    public void testStylishJson() throws Exception {
+        String actual = Differ.generate(jsonTreePathFile1, jsonTreePathFile2, "stylish");
+        assertThat(actual).isEqualTo(expectedStylish);
     }
 
     @Test
-    public void testTreeYml() throws Exception {
-        String actual = Differ.generate(ymlTreePathFile1, ymlTreePathFile2);
-        assertThat(actual).isEqualTo(expectedTreeYml);
+    public void testStylishYml() throws Exception {
+        String actual = Differ.generate(ymlTreePathFile1, ymlTreePathFile2, "stylish");
+        assertThat(actual).isEqualTo(expectedStylish);
+    }
+
+    @Test
+    public void testPlainJson() throws Exception {
+        String actual = Differ.generate(jsonTreePathFile1, jsonTreePathFile2, "plain");
+        assertThat(actual).isEqualTo(expectedPlain);
+    }
+
+    @Test
+    public void testPlainYml() throws Exception {
+        String actual = Differ.generate(ymlTreePathFile1, ymlTreePathFile2, "plain");
+        assertThat(actual).isEqualTo(expectedPlain);
     }
 }
